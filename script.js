00e1c81763735b56270f61f0d56504107a5b85cb
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-04-23 10:29:14 +0530

    created script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const todoList = [];
+
+function add(){
+    const inputElement = document.getElementById('input-todo');
+    todoList.push(inputElement.value);
+    inputElement.value = "";
+
+    renderToDoList();
+}
+
+function renderToDoList(){
+    const listContainer = document.getElementById('list-container');
+    listContainer.innerHTML = "";
+    for(const todoItem of todoList)
+    {
+        listContainer.innerHTML += `<div class="todoItem">
+            <input type="checkbox">
+            ${todoItem}</div>` ;
+    }
+}
