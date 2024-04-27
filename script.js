8c80f2f2b410df6797e977384fc698130213743d
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-04-27 10:42:07 +0530

    local storage added

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const todoList = [];
+let todoList = [];
+
+const listFromLocalStorage = localStorage.getItem("todoList");
+if(listFromLocalStorage)
+{
+    todoList = JSON.parse(listFromLocalStorage);
+    renderToDoList();
+}
 
 function add(){
     const inputElement = document.getElementById('input-todo');
     todoList.push(inputElement.value);
+    
+    localStorage.setItem("todoList", JSON.stringify(todoList));
     inputElement.value = "";
 
     renderToDoList();
@@ -11,6 +20,7 @@ function add(){
 function renderToDoList(){
     const listContainer = document.getElementById('list-container');
     listContainer.innerHTML = "";
+
     for(const todoItem of todoList)
     {
         listContainer.innerHTML += `<div class="todoItem">
